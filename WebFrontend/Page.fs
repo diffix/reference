bbe9a64ac59e42a05f11c393acc04e3b3992b1eb
Sebastian Probst Eide <sebastian@aircloak.com>
2020-12-03 18:17:25 +0100

    Fix footer

--- WebFrontend/Page.fs
+++ WebFrontend/Page.fs
@@ -48,9 +48,9 @@ let layout content =
           ]
         ]
         
-        div [_class "mt-8 py-5 text-gray-600"] [
+        div [_class "mt-4 mx-auto py-5 text-sm text-gray-600"] [
           str "For the source code and more info, checkout "
-          a [_href "https://github.com/diffix/prototype"; _class "underlined"] [str "Github"]
+          a [_href "https://github.com/diffix/prototype"; _class "underline"] [str "Github"]
           str "."
         ]
       ]
