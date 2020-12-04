c2b9416bcdbda6d31edc28516f0f472092518eba
Sebastian Probst Eide <sebastian@aircloak.com>
2020-12-04 17:20:29 +0100

    Only do LCF if requested

--- DiffixEngine/Anonymizer.fs
+++ DiffixEngine/Anonymizer.fs
@@ -12,25 +12,28 @@ let private randomNum (rnd: Random) mean stdDev =
 let private newRandom (reqParams: RequestParams) =
   Random(reqParams.Seed)
   
-let private lowCountFilter reqParams rnd (rows: AnonymizableRow list) =
-  let rowsToReject =
+let private lowCountFilter (reqParams: RequestParams) rnd (rows: AnonymizableRow list) =
+  match reqParams.LowCountSettings with
+  | None -> rows
+  | Some lowCountParams ->
+    let rowsToReject =
+      rows
+      |> List.groupBy(fun row -> row.Columns)
+      |> List.filter(fun (_columns, instancesOfRow) ->
+        let distinctUsersCount =
+          instancesOfRow
+          |> List.map(fun row -> row.AidValues)
+          |> Set.unionMany
+          |> Set.count
+        let lowCountThreshold = randomNum rnd lowCountParams.Threshold lowCountParams.StdDev
+        (float distinctUsersCount) < lowCountThreshold
+      )
+      |> List.map(fst)
+      |> Set.ofSeq
     rows
-    |> List.groupBy(fun row -> row.Columns)
-    |> List.filter(fun (_columns, instancesOfRow) ->
-      let distinctUsersCount =
-        instancesOfRow
-        |> List.map(fun row -> row.AidValues)
-        |> Set.unionMany
-        |> Set.count
-      let lowCountThreshold = randomNum rnd reqParams.LowCountThreshold reqParams.LowCountThresholdStdDev
-      (float distinctUsersCount) < lowCountThreshold
+    |> List.filter(fun row ->
+      not (Set.contains row.Columns rowsToReject)
     )
-    |> List.map(fst)
-    |> Set.ofSeq
-  rows
-  |> List.filter(fun row ->
-    not (Set.contains row.Columns rowsToReject)
-  )
   
 let anonymize reqParams (rows: AnonymizableRow list) =
   let rnd = newRandom reqParams
