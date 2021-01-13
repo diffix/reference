924b32590165b58d57ccef7eac49ea4a8fbb2fa0
Cristian Berneanu <bcristi@gmail.com>
2021-01-13 17:37:51 +0200

    Make the table model generic.

--- src/OpenDiffix.Core/QueryEngine.fs
+++ src/OpenDiffix.Core/QueryEngine.fs
@@ -1,22 +1,154 @@
 ﻿namespace OpenDiffix.Core
 
 module QueryEngine =
+  open System.Data.SQLite
   open FsToolkit.ErrorHandling
   open OpenDiffix.Core
   open OpenDiffix.Core.AnonymizerTypes
+  open OpenDiffix.Core.ParserTypes
+
+  type DbConnection = SQLite.DbConnection
+
+  let private getTables (connection: DbConnection) =
+    asyncResult {
+      let! tables = Table.getAll connection
+
+      return
+        {
+          Columns = [ "name" ]
+          Rows = tables |> List.map (fun table -> [ StringValue table.Name ])
+        }
+    }
+
+  let private getColumnsFromTable (connection: DbConnection) (TableName tableName) =
+    asyncResult {
+      let! tables = Table.getAll connection
+
+      let rows =
+        tables
+        |> List.tryFind (fun table -> table.Name = tableName)
+        |> function
+        | None -> []
+        | Some table ->
+            table.Columns
+            |> List.map (fun column -> //
+              [ StringValue column.Name; StringValue(Table.columnTypeToString column.Type) ]
+            )
+
+      return { Columns = [ "name"; "type" ]; Rows = rows }
+    }
+
+
+  let rec private expressionToSql =
+    function
+    | Constant (Integer value) -> string value
+    | Constant (String value) -> sprintf "'%s'" value
+    | Column (PlainColumn (ColumnName name)) -> name
+    | Column (AliasedColumn (ColumnName columnName, _aliasName)) -> columnName
+    | Function (functionName, subExpression) -> sprintf "%s(%s)" functionName (expressionToSql subExpression)
+    | AggregateFunction (AnonymizedCount (Distinct (ColumnName name))) -> sprintf "count(distinct %s)" name
+
+  let rec columnToSql index column = sprintf "%s as col%i" (expressionToSql column) index
+
+  let private tableName =
+    function
+    | Table (TableName tableName) -> tableName
+
+  let private generateSqlQuery aidColumnName (query: SelectQuery) =
+    let aidColumn = Column(PlainColumn aidColumnName)
+
+    let columns =
+      aidColumn :: query.Expressions
+      |> List.mapi columnToSql
+      |> List.reduce (sprintf "%s, %s")
+
+    $"""
+    SELECT
+      {columns}
+    FROM {tableName query.From}
+    """
+
+  let private readValue index (reader: SQLiteDataReader) =
+    if reader.IsDBNull index then
+      NullValue
+    else
+      match reader.GetFieldType(index) with
+      | fieldType when fieldType = typeof<int32> -> IntegerValue(reader.GetInt32 index)
+      | fieldType when fieldType = typeof<int64> -> IntegerValue(int (reader.GetInt64 index))
+      | fieldType when fieldType = typeof<string> -> StringValue(reader.GetString index)
+      | unknownType -> StringValue(sprintf "Unknown type: %A" unknownType)
+
+  let private readQueryResults (connection: DbConnection) aidColumnName (query: SelectQuery) =
+    asyncResult {
+      let! tables = Table.getAll connection
+      let desiredTableName = tableName query.From
+
+      let table = tables |> List.tryFind (fun table -> table.Name = desiredTableName)
+
+      if table.IsNone then
+        return! Error("Execution error: Unknown table " + desiredTableName)
+      else
+        let sqlQuery = generateSqlQuery aidColumnName query
+        printfn "Using query:\n%s" sqlQuery
+        use command = new SQLiteCommand(sqlQuery, connection)
+
+        try
+          let reader = command.ExecuteReader()
+
+          return
+            seq {
+              while reader.Read() do
+                let aidValue = readValue 0 reader
+                let rowValues = [ 1 .. reader.FieldCount - 1 ] |> List.map (fun index -> readValue index reader)
+
+                let row = { AidValues = Set.ofList [ aidValue ]; RowValues = rowValues }
+                yield row
+            }
+        with ex -> return! Error("Execution error: " + ex.Message)
+    }
+
+  let private extractAidColumn anonymizationParams ({ From = Table (TableName tableName) }: SelectQuery) =
+    match anonymizationParams.TableSettings.TryFind(tableName) with
+    | None
+    | Some { AidColumns = [] } -> Error "Execution error: An AID column name is required"
+    | Some { AidColumns = [ column ] } -> Ok column
+    | Some _ -> Error "Execution error: Multiple AID column names aren't supported yet"
+
+  let private columnName =
+    function
+    | Constant (Integer value) -> string value
+    | Constant (String value) -> value
+    | Column (PlainColumn (ColumnName columnName))
+    | Column (AliasedColumn (ColumnName columnName, _)) -> columnName
+    | Function (functionName, _expression) -> functionName
+    | AggregateFunction (AnonymizedCount (Distinct (ColumnName name))) -> "count"
+
+  let private executeSelect (connection: DbConnection) anonymizationParams query =
+    asyncResult {
+      let! aidColumn = extractAidColumn anonymizationParams query
+
+      let! rawRows =
+        readQueryResults connection (OpenDiffix.Core.ParserTypes.ColumnName aidColumn) query
+        |> AsyncResult.map Seq.toList
+
+      let rows = Anonymizer.anonymize anonymizationParams rawRows
+      let columns = query.Expressions |> List.map columnName
+
+      return { Columns = columns; Rows = rows }
+    }
 
   let private executeQuery reqParams queryAst =
     asyncResult {
-      let! connection = DiffixSqlite.dbConnection reqParams.DatabasePath
+      let! connection = SQLite.dbConnection reqParams.DatabasePath
       do! connection.OpenAsync() |> Async.AwaitTask
 
       let! result =
         match queryAst with
-        | ParserTypes.ShowTables -> DiffixSqlite.getTables connection
-        | ParserTypes.ShowColumnsFromTable table -> DiffixSqlite.getColumnsFromTable connection table
-        | ParserTypes.SelectQuery query -> DiffixSqlite.executeSelect connection reqParams.AnonymizationParams query
+        | ParserTypes.ShowTables -> getTables connection
+        | ParserTypes.ShowColumnsFromTable table -> getColumnsFromTable connection table
+        | ParserTypes.SelectQuery query -> executeSelect connection reqParams.AnonymizationParams query
         | ParserTypes.AggregateQuery _query ->
-            asyncResult { return! Error(InvalidRequest "Aggregate queries aren't supported yet") }
+            asyncResult { return! Error "Request error: Aggregate queries aren't supported yet" }
 
       do! connection.CloseAsync() |> Async.AwaitTask
       return result
