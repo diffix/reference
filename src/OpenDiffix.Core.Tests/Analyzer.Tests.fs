c1c77ba2ed1c07874e0c87d0c07823b4ffca6c23
Sebastian Probst Eide <sebastian@aircloak.com>
2021-01-25 17:22:04 +0100

    Simplify analyzer tests

--- src/OpenDiffix.Core.Tests/Analyzer.Tests.fs
+++ src/OpenDiffix.Core.Tests/Analyzer.Tests.fs
@@ -4,6 +4,28 @@ open Xunit
 open OpenDiffix.Core
 open OpenDiffix.Core.AnalyzerTypes
 
+let testTable: Table =
+  {
+    Name = "table"
+    Columns =
+      [
+        { Name = "str_col"; Type = StringType }
+        { Name = "int_col"; Type = IntegerType }
+        { Name = "float_col"; Type = FloatType }
+        { Name = "bool_col"; Type = BooleanType }
+      ]
+  }
+
+let defaultQuery =
+  {
+    Columns = []
+    Where = Boolean true |> Constant
+    From = Table testTable
+    GroupingSets = [ [] ]
+    Having = Boolean true |> Constant
+    OrderBy = []
+  }
+
 let testParsedQuery queryString callback (expected: Query) =
   let testResult =
     Parser.parse queryString
@@ -12,26 +34,13 @@ let testParsedQuery queryString callback (expected: Query) =
 
   assertOkEqual testResult expected
 
-module AnalyzeSelect =
-  let testTable: Table =
-    {
-      Name = "table"
-      Columns =
-        [
-          { Name = "str_col"; Type = StringType }
-          { Name = "int_col"; Type = IntegerType }
-          { Name = "float_col"; Type = FloatType }
-          { Name = "bool_col"; Type = BooleanType }
-        ]
-    }
-
-  [<Fact>]
-  let ``Analyze count(distinct col)`` () =
-    testParsedQuery
-      "SELECT count(distinct int_col) from table"
-      (Analyzer.transformQuery testTable)
-      (SelectQuery
-        {
+[<Fact>]
+let ``Analyze count(distinct col)`` () =
+  testParsedQuery
+    "SELECT count(distinct int_col) from table"
+    (Analyzer.transformQuery testTable)
+    (SelectQuery
+      { defaultQuery with
           Columns =
             [
               {
@@ -42,87 +51,77 @@ module AnalyzeSelect =
                 Alias = ""
               }
             ]
-          Where = Boolean true |> Constant
-          From = Table testTable
-          GroupingSets = [ [] ]
-          Having = Boolean true |> Constant
-          OrderBy = []
-        })
+      })
 
-  [<Fact>]
-  let ``Selecting columns from a table`` () =
-    testParsedQuery
-      "SELECT str_col, bool_col FROM table"
-      (Analyzer.transformQuery testTable)
-      (SelectQuery
-        {
+[<Fact>]
+let ``Selecting columns from a table`` () =
+  testParsedQuery
+    "SELECT str_col, bool_col FROM table"
+    (Analyzer.transformQuery testTable)
+    (SelectQuery
+      { defaultQuery with
           Columns =
             [
               { Expression = ColumnReference(0, StringType); Alias = "" }
               { Expression = ColumnReference(3, BooleanType); Alias = "" }
             ]
-          Where = Boolean true |> Constant
-          From = Table testTable
-          GroupingSets = [ [] ]
-          Having = Boolean true |> Constant
-          OrderBy = []
-        })
+      })
 
-  [<Fact>]
-  let ``SELECT with alias, function, aggregate, GROUP BY, and WHERE-clause`` () =
-    let query = @"
-    SELECT
-      int_col as colAlias,
-      float_col + int_col,
-      count(int_col)
-    FROM table
-    WHERE
-      int_col > 0 and int_col < 10
-    GROUP BY int_col, float_col + int_col, count(int_col)
-    HAVING count(int_col) > 1
-    "
+[<Fact>]
+let ``SELECT with alias, function, aggregate, GROUP BY, and WHERE-clause`` () =
+  let query = @"
+  SELECT
+    int_col as colAlias,
+    float_col + int_col,
+    count(int_col)
+  FROM table
+  WHERE
+    int_col > 0 and int_col < 10
+  GROUP BY int_col, float_col + int_col, count(int_col)
+  HAVING count(int_col) > 1
+  "
 
-    testParsedQuery
-      query
-      (Analyzer.transformQuery testTable)
-      (SelectQuery
-        {
-          Columns =
-            [
-              { Expression = ColumnReference(1, IntegerType); Alias = "colAlias" }
-              {
-                Expression =
-                  FunctionExpr(ScalarFunction Plus, [ ColumnReference(2, FloatType); ColumnReference(1, IntegerType) ])
-                Alias = ""
-              }
-              {
-                Expression =
-                  FunctionExpr(AggregateFunction(Count, AggregateOptions.Default), [ ColumnReference(1, IntegerType) ])
-                Alias = ""
-              }
-            ]
-          Where =
-            FunctionExpr
-              (ScalarFunction And,
-               [
-                 FunctionExpr(ScalarFunction Gt, [ ColumnReference(1, IntegerType); Constant(Value.Integer 0) ])
-                 FunctionExpr(ScalarFunction Lt, [ ColumnReference(1, IntegerType); Constant(Value.Integer 10) ])
-               ])
-          From = Table testTable
-          GroupingSets =
-            [
-              [
-                ColumnReference(1, IntegerType)
+  testParsedQuery
+    query
+    (Analyzer.transformQuery testTable)
+    (SelectQuery
+      {
+        Columns =
+          [
+            { Expression = ColumnReference(1, IntegerType); Alias = "colAlias" }
+            {
+              Expression =
                 FunctionExpr(ScalarFunction Plus, [ ColumnReference(2, FloatType); ColumnReference(1, IntegerType) ])
+              Alias = ""
+            }
+            {
+              Expression =
                 FunctionExpr(AggregateFunction(Count, AggregateOptions.Default), [ ColumnReference(1, IntegerType) ])
-              ]
+              Alias = ""
+            }
+          ]
+        Where =
+          FunctionExpr
+            (ScalarFunction And,
+             [
+               FunctionExpr(ScalarFunction Gt, [ ColumnReference(1, IntegerType); Constant(Value.Integer 0) ])
+               FunctionExpr(ScalarFunction Lt, [ ColumnReference(1, IntegerType); Constant(Value.Integer 10) ])
+             ])
+        From = Table testTable
+        GroupingSets =
+          [
+            [
+              ColumnReference(1, IntegerType)
+              FunctionExpr(ScalarFunction Plus, [ ColumnReference(2, FloatType); ColumnReference(1, IntegerType) ])
+              FunctionExpr(AggregateFunction(Count, AggregateOptions.Default), [ ColumnReference(1, IntegerType) ])
             ]
-          Having =
-            FunctionExpr
-              (ScalarFunction Gt,
-               [
-                 FunctionExpr(AggregateFunction(Count, AggregateOptions.Default), [ ColumnReference(1, IntegerType) ])
-                 Constant(Value.Integer 1)
-               ])
-          OrderBy = []
-        })
+          ]
+        Having =
+          FunctionExpr
+            (ScalarFunction Gt,
+             [
+               FunctionExpr(AggregateFunction(Count, AggregateOptions.Default), [ ColumnReference(1, IntegerType) ])
+               Constant(Value.Integer 1)
+             ])
+        OrderBy = []
+      })
