c82190daab29172c7b8554aef37b2d0da01a70c0
Cristian Berneanu <bcristi@gmail.com>
2021-01-25 11:28:37 +0200

    Query Planner implementation.

--- src/OpenDiffix.Core/Planner.fs
+++ src/OpenDiffix.Core/Planner.fs
@@ -3,4 +3,75 @@ module OpenDiffix.Core.Planner
 open OpenDiffix.Core.PlannerTypes
 open OpenDiffix.Core.AnalyzerTypes
 
-let plan (query: Query): Plan = failwith "not implemented"
+let private planFrom =
+  function
+  | Table table -> Plan.Scan table
+  | Join _ -> failwith "join planning not yet supported"
+  | Query _ -> failwith "subquery planning not yet supported"
+
+let private planProject expressions plan = Plan.Project(plan, expressions)
+
+let private planFilter condition plan =
+  match condition with
+  | Constant (Boolean true) -> plan
+  | _ -> Plan.Filter(plan, condition)
+
+let private planSort sortExpressions plan =
+  match sortExpressions with
+  | [] -> plan
+  | _ -> Plan.Sort(plan, sortExpressions)
+
+let rec private extractAggregators expression =
+  match expression with
+  | FunctionExpr (ScalarFunction _, args) -> args |> List.collect extractAggregators
+  | FunctionExpr (AggregateFunction _, _) as aggregator -> [ aggregator ]
+  | _ -> []
+
+let private planAggregate (groupingLabels: Expression list) (aggregators: Expression list) plan =
+  if groupingLabels.IsEmpty && aggregators.IsEmpty then plan else Plan.Aggregate(plan, groupingLabels, aggregators)
+
+let private getType expression = expression |> Expression.GetType |> Utils.unwrap
+
+let rec private projectExpression expression columns =
+  match columns |> List.tryFindIndex (fun column -> column = expression) with
+  | None ->
+      match expression with
+      | FunctionExpr (fn, args) ->
+          let args = args |> List.map (fun expression -> projectExpression expression columns)
+          FunctionExpr(fn, args)
+      | _ -> expression
+  | Some i -> ColumnReference(i, getType expression)
+
+let private planSelect query =
+  let selectedExpressions = query.Columns |> List.map (fun column -> column.Expression)
+  let orderByExpressions = query.OrderBy |> List.map (fun (expression, _, _) -> expression)
+  let expressions = selectedExpressions @ orderByExpressions
+
+  let aggregators = expressions |> List.collect extractAggregators |> List.distinct
+  let groupingLabels = query.GroupingSets |> List.concat |> List.distinct
+  let aggregatedColumns = groupingLabels @ aggregators
+
+  let selectedExpressions =
+    selectedExpressions
+    |> List.map (fun expression -> projectExpression expression aggregatedColumns)
+
+  let orderByExpressions =
+    query.OrderBy
+    |> List.map (fun (expression, direction, nulls) ->
+      let expression = projectExpression expression aggregatedColumns
+      expression, direction, nulls
+    )
+
+  let havingExpression = projectExpression query.Having aggregatedColumns
+
+  planFrom query.From
+  |> planFilter query.Where
+  |> planAggregate groupingLabels aggregators
+  |> planFilter havingExpression
+  |> planSort orderByExpressions
+  |> planProject selectedExpressions
+
+let plan =
+  function
+  | SelectQuery query -> planSelect query
+  | UnionQuery _ -> failwith "union planning not yet supported"
--- src/OpenDiffix.Core/Utils.fs
+++ src/OpenDiffix.Core/Utils.fs
@@ -2,3 +2,8 @@ namespace OpenDiffix.Core
 
 module Utils =
   let equalsI s1 s2 = System.String.Equals(s1, s2, System.StringComparison.CurrentCultureIgnoreCase)
+
+  let unwrap =
+    function
+    | Ok value -> value
+    | Error message -> failwith message
