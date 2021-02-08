f97bdf708323f0660ffeb8e277841bafe5f46752
Sebastian Probst Eide <sebastian@aircloak.com>
2021-02-08 15:24:25 +0100

    Filter garbage columns from query results before producing result

--- src/OpenDiffix.Core/QueryEngine.fs
+++ src/OpenDiffix.Core/QueryEngine.fs
@@ -12,23 +12,40 @@ module QueryEngine =
   open OpenDiffix.Core
   open OpenDiffix.Core.AnalyzerTypes
 
-  let rec private extractColumnNames =
+  let rec private extractColumns =
     function
-    | UnionQuery (_, query1, _query2) -> extractColumnNames query1
-    | SelectQuery query -> query.Columns |> List.map (fun column -> column.Alias)
+    | UnionQuery (_, query1, _query2) -> extractColumns query1
+    | SelectQuery query -> query.Columns
+
+  let rec private extractColumnNames columns =
+    columns
+    |> List.filter (fun column -> not column.Junk)
+    |> List.map (fun column -> column.Alias)
+
+  let removeJunkColumns columns (row: Row): Row =
+    row
+    |> Array.zip columns
+    |> Array.filter (fun (selectExpression, _value) -> not selectExpression.Junk)
+    |> Array.map snd
 
   let run connection statement anonParams =
     asyncResult {
       let! parsedQuery = Parser.parse statement
 
       let! query = Analyzer.analyze connection anonParams parsedQuery
+      let columns = extractColumns query
 
       let plan = Planner.plan query
 
       let context = { AnonymizationParams = anonParams }
-      let rows = plan |> Executor.execute connection context |> Seq.toList
 
-      let columns = extractColumnNames query
+      let rows =
+        plan
+        |> Executor.execute connection context
+        |> Seq.map (removeJunkColumns (Array.ofList columns))
+        |> Seq.toList
+
+      let columns = extractColumnNames columns
 
       return { Columns = columns; Rows = rows }
     }
