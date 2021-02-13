f5bec16b996c42deb5b1ae54d396c0391a551f54
Paul Francis <paul@francis.com>
2021-02-13 13:48:12 +0100

    initial draft

--- docs/lcfParams.py
+++ docs/lcfParams.py
@@ -1,20 +1,9 @@
 import random
+import pandas as pd
 
 # Measures how frequently different counts are suppressed by LCF
 # given the following parameters. Prints a markdown table with the
 # results.
-
-lower = 1.5
-mean_sd = [ 
-            [3,0.5],
-            [3.5,0.5],
-            [4,0.5],[4,0.7],[4,1.0],
-            [5,0.5],[5,1.0],[5,1.5],
-            [6,0.5],[6,1.0],[6,1.5], [6,2.0],
-            [7,0.5],[7,1.0],[7,1.5], [7,2.0]
-        ]
-counts = [1,2,3,4,5,6,7,8,9]
-trials = 100000
 
 def do_suppress(count,lower,upper,mean,sd):
     threshold = random.gauss(mean,sd)
@@ -25,121 +14,115 @@ def do_suppress(count,lower,upper,mean,sd):
     else:
         return False
 
-results = [{} for _ in range(len(mean_sd))]
-for i in range(len(mean_sd)):
-    mean = mean_sd[i][0]
-    sd = mean_sd[i][1]
-    upper = mean + (mean - lower)
+def do_basic_probs(lower,mean_sd,counts,trials):
+    results = {}
+    results['(mean,sd)'] = []
     for count in counts:
-        results[i][count] = 0
-        for _ in range(trials):
-            suppress = do_suppress(count,lower,upper,mean,sd)
-            if not suppress:
-                results[i][count] += 1
-
-print('''
-Given the count of distinct AIDs in a bucket, what is the probability that the bucket will be reported (not suppressed) (`lower=1.5`):
-        ''')
-# Make results markdown table
-print("Config    ",end='')
-for count in counts:
-    print(f"|  {count}     ",end='')
-print('')
-print("  ---     ",end='')
-for count in counts:
-    print(f"|  ---   ",end='')
-print('')
-for i in range(len(mean_sd)):
-    mean = mean_sd[i][0]
-    sd = mean_sd[i][1]
-    print(f"({mean:1.1f},{sd}) ",end='')
+        results[count] = [0 for _ in range(len(mean_sd))]
+    for i in range(len(mean_sd)):
+        mean = mean_sd[i][0]
+        sd = mean_sd[i][1]
+        upper = mean + (mean - lower)
+        results['(mean,sd)'].append(f"({mean:1.1f},{sd})")
+        for count in counts:
+            for _ in range(trials):
+                # Here we suppress without the hard lower limit, just to see
+                # how often the hard limit would be invoked
+                suppress = do_suppress(count,0,upper,mean,sd)
+                if not suppress:
+                    results[count][i] += 1
     for count in counts:
-        frac = results[i][count] / trials
-        print(f"| {frac:0.4f} ",end='')
-    print('')
+        for i in range(len(results[count])):
+            frac = results[count][i] / trials
+            results[count][i] = f"{frac:0.5f}"
+    
+    df = pd.DataFrame.from_dict(results)
 
-print('''
-Suppose that an attacker knows that there are either 1 or 2 AIDs in a bucket.  Suppose that the probability of either outcome is 50%. The following table shows three things:
-1. The probability that there is in fact 1 AID given that the bucket was suppressed.
-2. The probability that there are in fact 2 AIDs given that the bucket was not suppressed.
-3. The likelihood that the attacker will learn with certainty that there are 2 AIDs for any given bucket.
+    print(f'''
+Given the count of distinct AIDs in a bucket, what is the probability that the bucket will be reported (not suppressed) (`lower=0`):
         ''')
-
-print("Config    | Prob 1 AID (suppressed) | Prob 2 AIDs (reported) | Prob reported")
-print(" ---      | ---                     | ---                    | ---")
-for i in range(len(mean_sd)):
-    mean = mean_sd[i][0]
-    sd = mean_sd[i][1]
-    upper = mean + (mean - lower)
-    rightWhenSuppressed = 0
-    numSuppressed = 0
-    rightWhenReported = 0
-    numReported = 0
-    for _ in range(trials):
-        count = random.randrange(1,3)
-        suppress = do_suppress(count,lower,upper,mean,sd)
-        if suppress:
-            numSuppressed += 1
-            # Guess count = 1
-            if count == 1:
-                rightWhenSuppressed += 1
-        else:
-            numReported += 1
-            # Guess count = 2
-            if count == 2:
-                rightWhenReported += 1
-    fracRightWhenSuppressed = rightWhenSuppressed / numSuppressed
-    if numReported:
-        fracRightWhenReported = rightWhenReported / numReported
-        fracReported = int(trials / numReported)
-    else:
-        fracRightWhenReported = 1
-        fracReported = str(f"{trials}++")
-    print(f"({mean:1.1f},{sd}) ",end='')
-    print(f"| {fracRightWhenSuppressed:0.3f}                   ",end='')
-    print(f"| {fracRightWhenReported:0.2f}                   ",end='')
-    print(f"| 1/{fracReported}")
+    print(df.to_markdown(index=False))
 
 
-print('''
-Suppose that an attacker knows that there are either 2 or 3 AIDs in a bucket.  Suppose that the probability of either outcome is 50%. The following table shows three things:
-1. The probability that there are in fact 2 AIDs given that the bucket was suppressed.
-2. The probability that there are in fact 3 AIDs given that the bucket was reported.
-3. The probability that the bucket was reported
+def do_conditional_probs(lower,mean_sd,counts,trials):
+    res = {}
+    res['(mean,sd)'] = []
+    res['N'] = []
+    res['Prob N AIDs (sup)'] = []
+    res['Prob N+1 AIDs (rep)'] = []
+    res['Prob reported'] = []
+    for i in range(len(mean_sd)):
+        mean = mean_sd[i][0]
+        sd = mean_sd[i][1]
+        upper = mean + (mean - lower)
+        for n in range(1,int(mean)):
+            res['(mean,sd)'].append(f"({mean:1.1f},{sd})")
+            res['N'].append(f"{n}")
+            rightWhenSuppressed = 0
+            numSuppressed = 0
+            rightWhenReported = 0
+            numReported = 0
+            for _ in range(trials):
+                count = random.randrange(n,n+2)
+                suppress = do_suppress(count,lower,upper,mean,sd)
+                if suppress:
+                    numSuppressed += 1
+                    # Guess count = 1
+                    if count == n:
+                        rightWhenSuppressed += 1
+                else:
+                    numReported += 1
+                    # Guess count = 2
+                    if count == n+1:
+                        rightWhenReported += 1
+            fracRightWhenSuppressed = rightWhenSuppressed / numSuppressed
+            res['Prob N AIDs (sup)'].append(f"{fracRightWhenSuppressed:0.3f}")
+            if numReported:
+                fracRightWhenReported = rightWhenReported / numReported
+                fracReported = int(trials / numReported)
+                res['Prob N+1 AIDs (rep)'].append(f"{fracRightWhenReported:0.2f}")
+                res['Prob reported'].append(f"1/{fracReported}")
+            else:
+                res['Prob N+1 AIDs (rep)'].append('1')
+                res['Prob reported'].append(f"{trials}++")
+        res['(mean,sd)'].append(' ')
+        res['N'].append(' ')
+        res['Prob N AIDs (sup)'].append(' ')
+        res['Prob N+1 AIDs (rep)'].append(' ')
+        res['Prob reported'].append(' ')
+    
+    print(f'''
+Suppose that an attacker knows that there are either N or N+1 AIDs in a bucket.  Suppose that the probability of either outcome is 50%. The following table shows three things (lower={lower}):
+1. The probability that there is in fact N AIDs given that the bucket is suppressed.
+2. The probability that there are in fact N+1 AIDs given that the bucket is reported.
+3. The likelihood that the bucket is reported.
         ''')
+    df = pd.DataFrame.from_dict(res)
+    print(df.to_markdown(index=False))
 
-print("Config    | Prob 2 AIDs (suppressed) | Prob 3 AIDs (reported) | Prob reported")
-print(" ---      | ---                      | ---                    | ---")
-for i in range(len(mean_sd)):
-    mean = mean_sd[i][0]
-    sd = mean_sd[i][1]
-    upper = mean + (mean - lower)
-    rightWhenSuppressed = 0
-    numSuppressed = 0
-    rightWhenReported = 0
-    numReported = 0
-    for _ in range(trials):
-        count = random.randrange(2,4)
-        suppress = do_suppress(count,lower,upper,mean,sd)
-        if suppress:
-            numSuppressed += 1
-            # Guess count = 1
-            if count == 2:
-                rightWhenSuppressed += 1
-        else:
-            numReported += 1
-            # Guess count = 2
-            if count == 3:
-                rightWhenReported += 1
-    fracRightWhenSuppressed = rightWhenSuppressed / numSuppressed
-    if numReported:
-        fracRightWhenReported = rightWhenReported / numReported
-        fracReported = int(trials / numReported)
-    else:
-        fracRightWhenReported = 1
-        fracReported = str(f"{trials}++")
-    print(f"({mean:1.1f},{sd}) ",end='')
-    print(f"| {fracRightWhenSuppressed:0.3f}                    ",end='')
-    print(f"| {fracRightWhenReported:0.2f}                   ",end='')
-    print(f"| 1/{fracReported}")
+# -------------------------------------------------------------
 
+# Let's measure the following mean values (plus lower)
+base_means = [2,2.5,3,4,6,8]
+# Let's measure a factor of this many SDs between lower and mean
+sd_factors = [2,3,4,5]
+# Let's measure these values for lower
+lowers = [1.5,2.5]
+# Let's look at buckets with this many distinct AIDs (plus lower)
+base_counts = [0,1,2,3,4,5,6,7,8]
+trials = 1000000
+
+for lower in lowers:
+    counts = []
+    for base_count in base_counts:
+        counts.append(base_count + int(lower))
+    means = []
+    for base_mean in base_means:
+        means.append(base_mean + int(lower))
+    mean_sd = []
+    for mean in means:
+        for sd_factor in sd_factors:
+            sd = round((mean - int(lower)) / sd_factor,1)
+            mean_sd.append([mean,sd])
+    do_basic_probs(lower,mean_sd,counts,trials)
+    do_conditional_probs(lower,mean_sd,counts,trials)
