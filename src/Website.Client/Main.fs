3e6af43a2e3c571bc2a75f8c0c1c98d7c6fbf4a4
Sebastian Probst Eide <sebastian@aircloak.com>
2021-02-16 18:03:05 +0100

    Simple proof of concept running queries in browser

--- src/Website.Client/Main.fs
+++ src/Website.Client/Main.fs
@@ -5,113 +5,75 @@ open Elmish
 open Bolero
 open Bolero.Html
 open Bolero.Templating.Client
+open OpenDiffix.Core
+open OpenDiffix.Core.AnonymizerTypes
 
-/// Routing endpoints definition.
 type Page =
     | [<EndPoint "/">] Home
-    | [<EndPoint "/counter">] Counter
 
-/// The Elmish application's model.
 type Model =
     {
-        page: Page
-        counter: int
-        books: Book[] option
-        error: string option
-        username: string
-        password: string
-        signedInAs: option<string>
-        signInFailed: bool
-    }
-
-and Book =
-    {
-        title: string
-        author: string
-        publishDate: DateTime
-        isbn: string
+        Page: Page
+        AnonParams: AnonymizationParams
+        Query: string
+        Result: QueryResult option
+        ParserError: Parser.SqlParserError option
     }
 
 let initModel =
     {
-        page = Home
-        counter = 0
-        books = None
-        error = None
-        username = ""
-        password = ""
-        signedInAs = None
-        signInFailed = false
+        Page = Home
+        AnonParams = AnonymizationParams.Default
+        Query = ""
+        Result = None
+        ParserError = None
     }
 
-/// The Elmish application's update messages.
 type Message =
     | SetPage of Page
-    | Increment
-    | Decrement
-    | SetCounter of int
-    | Error of exn
-    | ClearError
+    | SetQuery of string
+    | SetQueryResult of Result<QueryResult, Parser.SqlParserError>
 
 let update message model =
     match message with
     | SetPage page ->
-        { model with page = page }, Cmd.none
+        { model with Page = page }, Cmd.none
+    | SetQuery query ->
+        let cmd =
+          Cmd.OfAsync.result (async {
+            let dataProvider = DataProvider.PlaygroundDataProvider()
+            let! result = QueryEngine.run dataProvider query model.AnonParams
+            return SetQueryResult result
+          })
+        { model with Query = query }, cmd
+    | SetQueryResult result ->
+        let updatedModel =
+          match result with
+          | Ok result ->
+            { model with Result = Some result; ParserError = None }
+          | Error parseError ->
+            { model with ParserError = Some parseError }
+        updatedModel, Cmd.none
 
-    | Increment ->
-        { model with counter = model.counter + 1 }, Cmd.none
-    | Decrement ->
-        { model with counter = model.counter - 1 }, Cmd.none
-    | SetCounter value ->
-        { model with counter = value }, Cmd.none
-
-    | Error exn ->
-        { model with error = Some exn.Message }, Cmd.none
-    | ClearError ->
-        { model with error = None }, Cmd.none
-
-/// Connects the routing system to the Elmish application.
-let router = Router.infer SetPage (fun model -> model.page)
+let router = Router.infer SetPage (fun model -> model.Page)
 
 type Main = Template<"wwwroot/main.html">
 
-let homePage _model _dispatch =
-    Main.Home().Elt()
-
-let counterPage model dispatch =
-    Main.Counter()
-        .Decrement(fun _ -> dispatch Decrement)
-        .Increment(fun _ -> dispatch Increment)
-        .Value(model.counter, fun v -> dispatch (SetCounter v))
-        .Elt()
-
-let menuItem (model: Model) (page: Page) (text: string) =
-    Main.MenuItem()
-        .Active(if model.page = page then "is-active" else "")
-        .Url(router.Link page)
-        .Text(text)
-        .Elt()
+let homePage model dispatch =
+    Main
+      .Home()
+      .Query(model.Query, fun query -> dispatch (SetQuery query))
+      .Result(
+        match model.Result, model.ParserError with
+        | Some result, _ -> $"has result %A{result}"
+        | _, Some parseError -> $"has parse error %A{parseError}"
+        | _, _ -> "Does not yet have meaningful results or state"
+      )
+      .Elt()
 
 let view model dispatch =
     Main()
-        .Menu(concat [
-            menuItem model Home "Home"
-            menuItem model Counter "Counter"
-        ])
-        .Body(
-            cond model.page <| function
-            | Home -> homePage model dispatch
-            | Counter -> counterPage model dispatch
-        )
-        .Error(
-            cond model.error <| function
-            | None -> empty
-            | Some err ->
-                Main.ErrorNotification()
-                    .Text(err)
-                    .Hide(fun _ -> dispatch ClearError)
-                    .Elt()
-        )
+        .Body(homePage model dispatch)
         .Elt()
 
 type MyApp() =
