376101b1b7ef1625b1cf14effb06eacb36cf73e0
Sebastian Probst Eide <sebastian@aircloak.com>
2021-03-31 11:56:09 +0200

    Anonymizer formatted

--- src/OpenDiffix.Core/Anonymizer.fs
+++ src/OpenDiffix.Core/Anonymizer.fs
@@ -27,10 +27,7 @@ let private noiseValue rnd (noiseParam: NoiseParam) =
   |> int32
 
 let countAids (aidSets: Set<AidHash> array option) (anonymizationParams: AnonymizationParams) =
-  let aidSet =
-    if aidSets.IsNone
-    then failwith "Expecting AID sets for count"
-    else aidSets.Value |> Array.head
+  let aidSet = if aidSets.IsNone then failwith "Expecting AID sets for count" else aidSets.Value |> Array.head
 
   let rnd = newRandom aidSet anonymizationParams
   let noise = noiseValue rnd anonymizationParams.Noise
@@ -40,54 +37,52 @@ let isLowCount (aidSets: Set<AidHash> array option) (anonymizationParams: Anonym
   match aidSets with
   | None -> true
   | Some aidSets ->
-    aidSets
-    |> Array.map(fun aidSet ->
-      let rnd = newRandom aidSet anonymizationParams
-      let threshold =
-        randomUniform
-          rnd
-          {
-            Lower = anonymizationParams.MinimumAllowedAids
-            Upper = anonymizationParams.MinimumAllowedAids + 2
-          }
+      aidSets
+      |> Array.map (fun aidSet ->
+        let rnd = newRandom aidSet anonymizationParams
 
-      aidSet.Count < threshold
-    )
-    |> Array.reduce (||)
+        let threshold =
+          randomUniform
+            rnd
+            {
+              Lower = anonymizationParams.MinimumAllowedAids
+              Upper = anonymizationParams.MinimumAllowedAids + 2
+            }
+
+        aidSet.Count < threshold
+      )
+      |> Array.reduce (||)
 
 let count (anonymizationParams: AnonymizationParams) (perUserContributions: Map<AidHash, int64> array option) =
   match perUserContributions with
   | None -> Null
   | Some perUserContributions ->
-    let perUserContribution =
-      perUserContributions
-      |> Array.head
-      |> Map.toList
+      let perUserContribution = perUserContributions |> Array.head |> Map.toList
 
-    let aids = perUserContribution |> List.map fst |> Set.ofList
-    let rnd = newRandom aids anonymizationParams
-    // The noise value must be generated first to make sure the random number generator is fresh.
-    // This ensures count(distinct aid) which uses addNoise directly produces the same results.
-    let noise = noiseValue rnd anonymizationParams.Noise
+      let aids = perUserContribution |> List.map fst |> Set.ofList
+      let rnd = newRandom aids anonymizationParams
+      // The noise value must be generated first to make sure the random number generator is fresh.
+      // This ensures count(distinct aid) which uses addNoise directly produces the same results.
+      let noise = noiseValue rnd anonymizationParams.Noise
 
-    let sortedUserContributions = perUserContribution |> List.map snd |> List.sortDescending
+      let sortedUserContributions = perUserContribution |> List.map snd |> List.sortDescending
 
-    let outlierCount = randomUniform rnd anonymizationParams.OutlierCount
-    let topCount = randomUniform rnd anonymizationParams.TopCount
+      let outlierCount = randomUniform rnd anonymizationParams.OutlierCount
+      let topCount = randomUniform rnd anonymizationParams.TopCount
 
-    if sortedUserContributions.Length < outlierCount + topCount then
-      Null
-    else
-      let topValueSummed =
-        sortedUserContributions
-        |> List.skip outlierCount
-        |> List.take topCount
-        |> List.sum
+      if sortedUserContributions.Length < outlierCount + topCount then
+        Null
+      else
+        let topValueSummed =
+          sortedUserContributions
+          |> List.skip outlierCount
+          |> List.take topCount
+          |> List.sum
 
-      let topValueAverage = (float topValueSummed) / (float topCount)
-      let outlierReplacement = topValueAverage * (float outlierCount) |> int64
+        let topValueAverage = (float topValueSummed) / (float topCount)
+        let outlierReplacement = topValueAverage * (float outlierCount) |> int64
 
-      let sumExcludingOutliers = sortedUserContributions |> List.skip (outlierCount) |> List.sum
+        let sumExcludingOutliers = sortedUserContributions |> List.skip (outlierCount) |> List.sum
 
-      let totalCount = sumExcludingOutliers + outlierReplacement
-      (max (totalCount + int64 noise) 0L) |> Integer
+        let totalCount = sumExcludingOutliers + outlierReplacement
+        (max (totalCount + int64 noise) 0L) |> Integer
