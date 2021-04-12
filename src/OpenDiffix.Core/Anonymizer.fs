a7d68dd256036e488153489bef8f62b92cb09c55
Sebastian Probst Eide <sebastian@aircloak.com>
2021-04-12 15:07:13 +0200

    Remove redundant parenthesis

--- src/OpenDiffix.Core/Anonymizer.fs
+++ src/OpenDiffix.Core/Anonymizer.fs
@@ -9,7 +9,7 @@ let private randomNormal (rnd: Random) stdDev =
   let u1 = 1.0 - rnd.NextDouble()
   let u2 = 1.0 - rnd.NextDouble()
 
-  let randStdNormal = Math.Sqrt(-2.0 * log (u1)) * Math.Sin(2.0 * Math.PI * u2)
+  let randStdNormal = Math.Sqrt(-2.0 * log u1) * Math.Sin(2.0 * Math.PI * u2)
 
   stdDev * randStdNormal
 
